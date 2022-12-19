439275d6491522dc8dc35a70a3ad835474f76283
Atsen10 <samuelatsenbizness@gmail.com>
2022-12-19 12:46:31 -0800

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes the pointer and update the value
+ * @n: int to check
+ * owned by atsen ict
+ * Return: 0 is success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
