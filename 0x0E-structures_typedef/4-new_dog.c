eca9836d166d1c950c85e4b8ca688fef6be1777d
Atsen10 <samuelatsenbizness@gmail.com>
2023-01-17 16:44:59 -0800

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,7 +29,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i < nl; i++)
-		;
 		dog->name[i] = name[i];
 	dog->age = age;
 	for (ol = 0; owner[ol]; ol++)
